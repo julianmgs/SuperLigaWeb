1a07ec076f875d9ee506077dc759c088098c1f75
Julian Pagano <julianmgs@gmail.com>
2017-12-19 14:54:05 -0300

    Fix en clase "active" del menu.

--- src/main/webapp/resources/js/site.js
+++ src/main/webapp/resources/js/site.js
@@ -5,26 +5,12 @@
     $('.dropdown-button').dropdown();
     $('.tooltipped').tooltip({delay: 50});
     
+    var pgurl = window.location.pathname;
+    $(".nav li a").each(function(){
+    	if($(this).attr("href") == pgurl || $(this).attr("href") == '' ) {
+    		$(this).parent().addClass("active");
+        }
+    })
+    
   }); // end of document ready
 })(jQuery); // end of jQuery name space
-
-/*
-$(document).ready(function () {
-    var url = window.location;
-    $('ul.nav a[href="' + url + '"]').parent().addClass('active');
-    $('ul.nav a').filter(function () {
-        return this.href == url;
-    }).parent().addClass('active');
-});
-*/
-
-/*
-$(function() {
-    var pgurl = window.location.href.substr(window.location.href
-.lastIndexOf("/")+1);
-    $("#nav ul li a").each(function(){
-         if($(this).attr("href") == pgurl || $(this).attr("href") == '' )
-         $(this).addClass("active");
-    })
-});
-*/
